c33df6de5a6f64c8afe435638d9b174fdd50b35d
Benjamin Carpenter <carpen77@purdue.edu>
2019-10-23 12:38:12 -0400

    Add files via upload
    Will need paraview python installed on your machine for this to work, and need to update the batch file to fit your needs.

---
+++ pvpython.bat
@@ -0,0 +1,3 @@
+set PATH=C:\Program Files\ParaView 5.6.0-Windows-msvc2015-64bit\bin;%PATH%
+set VTK_DATA_ROOT=\\techwin.ecn.purdue.edu\Classes\CGT\CGT 21500\Students Fall 2016\VTKData-7.1.1\Data
+pvpython.exe %1
